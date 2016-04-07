741f9ef031e2f926fd2b73b296b4263917f7f469
Yaman Umuroglu <yamanu@xilinx.com>
2016-04-07 16:25:12 +0100

    binarized fully-connected layer as used in BinaryNet

--- tiny_dnn/tiny_dnn.h
+++ tiny_dnn/tiny_dnn.h
@@ -16,6 +16,7 @@
 #include "tiny_dnn/core/framework/device.h"
 #include "tiny_dnn/core/framework/program_manager.h"
 #include "layers/batchnorm_layer.h"
+#include "layers/binarized_fc_layer.h"
 
 #include "tiny_dnn/layers/arithmetic_layer.h"
 #include "tiny_dnn/layers/average_pooling_layer.h"
